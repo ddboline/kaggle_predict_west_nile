bbde157b40e3bd74c6d9999c1e03f36abe119f8b
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-04 12:54:15 -0400

    ...

--- feature_extraction.py
+++ feature_extraction.py
@@ -18,14 +18,14 @@ import pandas as pd
 
 from dateutil.parser import parse
 
-WEATHER_VARS_WITH_M_T = (u'tmax', u'tmin', u'tavg', u'depart', u'dewpoint', 
-                         u'wetbulb', u'heat', u'cool', u'snowfall', 
-                         u'preciptotal', u'stnpressure', u'sealevel', 
-                         u'resultspeed', u'resultdir', u'avgspeed')
+WEATHER_VARS_WITH_M_T = (u'Tmax', u'Tmin', u'Tavg', u'Depart', u'DewPoint',
+                         u'WetBulb', u'Heat', u'Cool', u'Snowfall',
+                         u'PrecipTotal', u'StnPressure', u'SeaLevel',
+                         u'ResultSpeed', u'ResultDir', u'AvgSpeed', u'Water1')
 
-WEATHER_PHENOMENA = ('BCFG', 'BLDU', 'BLSN', 'BR', 'DU', 'DZ', 'FG', 'FG+', 
-                     'FU', 'FZDZ', 'FZFG', 'FZRA', 'GR', 'GS', 'HZ', 'MIFG', 
-                     'PL', 'PRFG', 'RA', 'SG', 'SN', 'SQ', 'TS', 'TSRA', 
+WEATHER_PHENOMENA = ('BCFG', 'BLDU', 'BLSN', 'BR', 'DU', 'DZ', 'FG', 'FG+',
+                     'FU', 'FZDZ', 'FZFG', 'FZRA', 'GR', 'GS', 'HZ', 'MIFG',
+                     'PL', 'PRFG', 'RA', 'SG', 'SN', 'SQ', 'TS', 'TSRA',
                      'TSSN', 'UP', 'VCFG', 'VCTS')
 
 def haversine_distance(lat1, lon1, lat2, lon2):
@@ -36,72 +36,118 @@ def haversine_distance(lat1, lon1, lat2, lon2):
     lat2 *= np.pi/180.
     dist = 2. * r_earth * np.arcsin(
                             np.sqrt(
-                                np.sin(dlat/2.)**2 + 
-                                    np.cos(lat1) * np.cos(lat2) * 
+                                np.sin(dlat/2.)**2 +
+                                    np.cos(lat1) * np.cos(lat2) *
                                     np.sin(dlon/2.)**2))
     return dist
 
+def lat_lon_box(lat, lon, dist):
+    r_earth = 6371.
+    d_2r = dist/(2.*r_earth)
+    dlat = 2. * (d_2r)
+    dlon = 2. * np.arcsin((np.sin(d_2r))/(np.cos(lat)))
+    dlat *= 180./np.pi
+    dlon *= 180./np.pi
+    return abs(dlat), abs(dlon)
+
 def feature_extraction():
     spray_df = pd.read_csv('spray.csv.gz', compression='gzip')
-    weather_df = pd.read_csv('weather.csv.gz', compression='gzip')
-#    train_df = pd.read_csv('train.csv.gz', compression='gzip')
-#    test_df = pd.read_csv('test.csv.gz', compression='gzip')
-    
+
     spray_lat_lon_list = []
     for idx, row in spray_df.iterrows():
         spray_lat_lon_list.append((row['Latitude'], row['Longitude']))
 
     weather_features = []
-    weather_labels = list(weather_df.columns)
-    for idx, row in weather_df.iterrows():
-        rowdict = {k: row[k] for k in weather_labels}
-        for k in WEATHER_VARS_WITH_M_T:
-            if k in rowdict:
-                if rowdict[k] == 'M':
-                    rowdict[k] = np.nan
-                elif rowdict[k] == 'T':
-                    rowdict[k] = 0.0
-        for k in rowdict:
-            if rowdict[k] == '-':
-                rowdict[k] = np.nan
-        for ph in WEATHER_PHENOMENA:
-            rowdict['wp%s' % ph] = 0
-        for ph in rowdict['CodeSum'].split():
-            if ph in WEATHER_PHENOMENA:
-                rowdict['wp%s' % ph] = 1
-        weather_features.append(rowdict)
+    cumu_labels = ('Tmax', 'Tmin', 'PrecipTotal')
+    cumu_features = {}
+    cumu_total = 0
+    current_year = -1
+    with gzip.open('weather.csv.gz', 'r') as wfile:
+        wcsv = csv.reader(wfile)
+        weather_labels = next(wcsv)
+        for row in wcsv:
+            rowdict = dict(zip(weather_labels, row))
+            rowdict['Date'] = parse(rowdict['Date'])
+            current_date = rowdict['Date']
+            if current_date.year != current_year:
+                current_year = current_date.year
+                cumu_features = {k: 0 for k in cumu_labels}
+                cumu_total = 0
+            for k in WEATHER_VARS_WITH_M_T:
+                if k in rowdict:
+                    rowdict[k] = rowdict[k].replace('M', 'nan')
+                    rowdict[k] = rowdict[k].replace('T', '0.0')
+            for k in rowdict:
+                if rowdict[k] == '-':
+                    rowdict[k] = 'nan'
+                if type(rowdict[k]) == str:
+                    rowdict[k] = rowdict[k].strip()
+            for ph in WEATHER_PHENOMENA:
+                rowdict['wp%s' % ph] = '0'
+            for ph in rowdict['CodeSum'].split():
+                if ph in WEATHER_PHENOMENA:
+                    rowdict['wp%s' % ph] = '1'
+            for lab in cumu_labels:
+                _tmp = float(rowdict[lab])
+                if not np.isnan(_tmp):
+                    cumu_features[lab] += _tmp
+            cumu_total += 1
+            for lab in ('Tmax', 'Tmin', 'PrecipTotal'):
+                rowdict['%s_cumu' % lab] = cumu_features[lab] / cumu_total
+            weather_features.append(rowdict)
+#            print('\n'.join(['%s: %s' % (k, rowdict[k]) for k in rowdict]))
+#            exit(0)
     for ph in WEATHER_PHENOMENA:
         weather_labels.append('wp%s' % ph)
+    for lab in cumu_labels:
+        weather_labels.append('%s_cumu' % lab)
+
 
     for prefix in 'train', 'test':
         with gzip.open('%s.csv.gz' % prefix, 'rb') as csvfile:
             outfile = gzip.open('%s_full.csv.gz' % prefix, 'wb')
             csv_reader = csv.reader(csvfile)
             labels = next(csv_reader)
-            
-            out_labels = labels + ['n_spray'] + weather_labels
+
+            out_labels = labels +\
+                         ['n_spray_%d' % x for x in range(1,11)]
+            for lab in weather_labels:
+                if lab == 'Date':
+                    continue
+                out_labels.append(lab)
+
             csv_writer = csv.writer(outfile)
             csv_writer.writerow(out_labels)
-            
+
             for idx, row in enumerate(csv_reader):
                 if idx % 1000 == 0:
                     print('processed %d' % idx)
+#                if idx > 100:
+#                    exit(0)
                 row_dict = dict(zip(labels, row))
-                
+
                 current_date = parse(row_dict['Date'])
                 cur_lat = float(row_dict['Latitude'])
                 cur_lon = float(row_dict['Longitude'])
-                
-                row_dict['n_spray'] = 0
-                for slat, slon in spray_lat_lon_list:
-                    sdist = haversine_distance(cur_lat, cur_lon, slat, slon)
-                    if sdist < 2.0:
-                        row_dict['n_spray'] += 1
 
+                for idx in range(1, 11):
+                    row_dict['n_spray_%d' % idx] = 0
+                dlat, dlon = lat_lon_box(cur_lat, cur_lon, 1.5)
+                for slat, slon in spray_lat_lon_list:
+#                    print(dlat, dlon, abs(slat-cur_lat), abs(slon-cur_lon))
+                    if abs(slat-cur_lat) > dlat or abs(slon-cur_lon) > dlon:
+                        continue
+                    sdist = haversine_distance(cur_lat, cur_lon, slat, slon)
+                    for idx in range(1,11):
+                        if sdist < idx/10.0:
+                            row_dict['n_spray_%d' % idx] += 1
+
+                for lab in ['Tmax_cumu', 'Tmin_cumu', 'PrecipTotal_cumu']:
+                    row_dict[lab] = 0
                 most_recent = 1000000
                 most_recent_w = weather_features[0]
                 for wfeat in weather_features:
-                    wdate = parse(wfeat['Date'])
+                    wdate = wfeat['Date']
                     if current_date.year != wdate.year:
                         continue
                     wdur = abs((current_date - wdate).days)
@@ -114,7 +160,9 @@ def feature_extraction():
                     row_dict[lab] = most_recent_w[lab]
                 row_val = [row_dict[col] for col in out_labels]
                 csv_writer.writerow(row_val)
-                print(row_dict)
+#                outfile.flush()
+#                print('\n'.join(['%s: %s' % (k, row_dict[k]) for k in row_dict]))
+#                exit(0)
     return
 
 if __name__ == '__main__':
--- load_data.py
+++ load_data.py
@@ -5,20 +5,87 @@ Created on Wed Apr 29 01:29:17 2015
 
 @author: ddboline
 """
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import pandas as pd
 
-def load_data():
-    train_df = pd.read_csv('train.csv.gz', compression='gzip')
-    test_df = pd.read_csv('test.csv.gz', compression='gzip')
+SPECIES = ['CULEX ERRATICUS', 'CULEX PIPIENS', 'CULEX PIPIENS/RESTUANS',
+           'CULEX RESTUANS', 'CULEX SALINARIUS', 'CULEX TARSALIS',
+           'CULEX TERRITANS']
+
+#TRAPS = ['T001', 'T002', 'T003', 'T004', 'T005', 'T006', 'T007', 'T008',
+#         'T009', 'T011', 'T012', 'T013', 'T014', 'T015', 'T016', 'T017',
+#         'T018', 'T019', 'T025', 'T027', 'T028', 'T030', 'T031', 'T033',
+#         'T034', 'T035', 'T036', 'T037', 'T039', 'T040', 'T043', 'T044',
+#         'T045', 'T046', 'T047', 'T048', 'T049', 'T050', 'T051', 'T054',
+#         'T054C', 'T060', 'T061', 'T062', 'T063', 'T065', 'T066', 'T067',
+#         'T069', 'T070', 'T071', 'T072', 'T073', 'T074', 'T075', 'T076',
+#         'T077', 'T078', 'T079', 'T080', 'T081', 'T082', 'T083', 'T084',
+#         'T085', 'T086', 'T088', 'T089', 'T090', 'T091', 'T092', 'T094',
+#         'T094B', 'T095', 'T096', 'T097', 'T099', 'T100', 'T102', 'T103',
+#         'T107', 'T114', 'T115', 'T128', 'T129', 'T135', 'T138', 'T141',
+#         'T142', 'T143', 'T144', 'T145', 'T146', 'T147', 'T148', 'T149',
+#         'T150', 'T151', 'T152', 'T153', 'T154', 'T155', 'T156', 'T157',
+#         'T158', 'T159', 'T160', 'T161', 'T162', 'T200', 'T206', 'T209',
+#         'T212', 'T215', 'T218', 'T219', 'T220', 'T221', 'T222', 'T223',
+#         'T224', 'T225', 'T226', 'T227', 'T228', 'T229', 'T230', 'T231',
+#         'T232', 'T233', 'T235', 'T236', 'T237', 'T238', 'T900', 'T903',
+#         'T002A', 'T002B', 'T065A', 'T090A', 'T090B', 'T090C', 'T128A',
+#         'T200A', 'T200B', 'T218A', 'T218B', 'T218C', 'T234']
+
+def clean_data(df):
+    df['Species'] = df['Species'].map({k: n for (n, k) in enumerate(SPECIES)})
+    df['is_sat_trap'] = df['Trap'].apply(lambda x: len(x[4:]) > 0).astype(int)
+    df['Trap'] = df['Trap'].apply(lambda x: x[1:4]).astype(int)
+
+    df = df.drop(labels=['Date', 'Date.1', 'Address', 'Street',
+                         'AddressNumberAndStreet', 'CodeSum', 'SnowFall',
+                         'Water1', 'Station', 'Depth', 'wpBCFG', 'wpBLDU',
+                         'wpBLSN', 'wpDU', 'wpFG+', 'wpFU', 'wpFZDZ',
+                         'wpFZFG', 'wpFZRA', 'wpGR', 'wpGS', 'wpMIFG', 'wpPL',
+                         'wpPRFG', 'wpSG', 'wpSN', 'wpSQ', 'wpTSSN', 'wpUP',
+                         'wpVCFG', 'AddressAccuracy'], axis=1)
+    return df
+
+def load_data(do_plots=False):
+    train_df = pd.read_csv('train_full.csv.gz', compression='gzip',
+                           low_memory=False)
+    test_df = pd.read_csv('test_full.csv.gz', compression='gzip',
+                          low_memory=False)
     submit_df = pd.read_csv('sampleSubmission.csv.gz', compression='gzip')
-    
-    
-    print train_df.columns
-    print test_df.columns
-    
-    print train_df.dtypes
-    print test_df.dtypes
-    return
+
+    train_df = clean_data(train_df)
+    test_df = clean_data(test_df)
+
+#    print(train_df.columns)
+#    print(test_df.columns)
+#    print(submit_df.columns)
+
+    print(train_df.dtypes)
+    print(test_df.dtypes)
+    print(submit_df.dtypes)
+
+#    for col in test_df.columns:
+#        if test_df[col].dtype == object:
+#            print(col, test_df[col].dtype)
+#    print(sorted(train_df['is_sat_trap'].unique()))
+#    print(sorted(train_df['Trap'].unique()))
+    if do_plots:
+        from plot_data import plot_data
+        plot_data(train_df, prefix='train_html')
+        plot_data(test_df, prefix='test_html')
+
+    xtrain = train_df.drop(labels=['NumMosquitos', 'WnvPresent'],
+                           axis=1).values
+    ytrain = train_df[['NumMosquitos', 'WnvPresent']].values
+    xtest = test_df.values
+    ytest = submit_df
+    return xtrain, ytrain, xtest, ytest
 
 if __name__ == '__main__':
-    load_data()
+    xtrain, ytrain, xtest, ytest = load_data(do_plots=False)
+
+    print([df.shape for df in (xtrain, ytrain, xtest, ytest)])
